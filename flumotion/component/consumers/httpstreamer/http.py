ylatuya/Flumotion

commit 49c08957a48e6c0f2e73b389a39f3768c1b6ec73

* flumotion/component/consumers/httpstreamer/http.py:       Throttle uiState updates in the streamer.
git-svn-id: svn+ssh://code.fluendo.com/flumotion/flumotion/trunk@5661 2b0047a9-a6d8-0310-accf-f7200b2a168c

flumotion/component/consumers/httpstreamer/http.py
@@ -54,6 +54,9 @@ __all__ = ['HTTPMedium', 'MultifdSinkStreamer']
 
 STATS_POLL_INTERVAL = 10
 
+UI_UPDATE_THROTTLE_PERIOD = 2.0 # Don't update UI more than once every two 
+                                # seconds
+
 # FIXME: generalize this class and move it out here ?
 class Stats:
     def __init__(self, sink):
@@ -292,6 +295,8 @@ class MultifdSinkStreamer(feedcomponent.ParseLaunchComponent, Stats):
         self._tport = None
 
         self._updateCallLaterId = None
+        self._lastUpdate = 0
+        self._updateUI_DC = None
 
         self._pending_removals = {}
 
@@ -585,12 +590,33 @@ class MultifdSinkStreamer(feedcomponent.ParseLaunchComponent, Stats):
         sink.emit('remove', fd)
 
     def update_ui_state(self):
+        """
+        Update the uiState object.
+        Such updates (through this function) are throttled to a maximum rate,
+        to avoid saturating admin clients with traffic when many clients are
+        connecting/disconnecting.
+        """
         def set(k, v):
             if self.uiState.get(k) != v:
                 self.uiState.set(k, v)
-        # fixme: have updateState just update what changed itself
-        # without the hack above
-        self.updateState(set)
+        now = time.time()
+        self._updateUI_DC = None
+
+        # If we haven't updated too recently, do it immediately.
+        if now - self._lastUpdate >= UI_UPDATE_THROTTLE_PERIOD:
+            if self._updateUI_DC:
+                self._updateUI_DC.cancel()
+                self._updateUI_DC = None
+
+            self._lastUpdate = now
+            # fixme: have updateState just update what changed itself
+            # without the hack above
+            self.updateState(set)
+        elif not self._updateUI_DC:
+            # Otherwise, schedule doing this in a few seconds (unless an update
+            # was already scheduled)
+            self._updateUI_DC = reactor.callLater(UI_UPDATE_THROTTLE_PERIOD,
+                self.update_ui_state)
 
     def _client_added_handler(self, sink, fd):
         self.log('[fd %5d] client_added_handler', fd)

commit 76f58473e916e831b40d9c74b130f79e3883c9cc

* flumotion/component/consumers/httpstreamer/http.py 	(MultifdSinkStreamer.update_ui_state): 	Don't erase delayed call id if the method wasn't called by the 	reactor from a timeout - fixes memory leak introduced in r5661.
git-svn-id: svn+ssh://code.fluendo.com/flumotion/flumotion/trunk@6168 2b0047a9-a6d8-0310-accf-f7200b2a168c

flumotion/component/consumers/httpstreamer/http.py
@@ -610,8 +610,12 @@ class MultifdSinkStreamer(feedcomponent.ParseLaunchComponent, Stats):
         def set(k, v):
             if self.uiState.get(k) != v:
                 self.uiState.set(k, v)
+
+        def update_ui_state_later():
+            self._updateUI_DC = None
+            self.update_ui_state()
+
         now = time.time()
-        self._updateUI_DC = None
 
         # If we haven't updated too recently, do it immediately.
         if now - self._lastUpdate >= UI_UPDATE_THROTTLE_PERIOD:
@@ -627,7 +631,7 @@ class MultifdSinkStreamer(feedcomponent.ParseLaunchComponent, Stats):
             # Otherwise, schedule doing this in a few seconds (unless an update
             # was already scheduled)
             self._updateUI_DC = reactor.callLater(UI_UPDATE_THROTTLE_PERIOD,
-                self.update_ui_state)
+                                                  update_ui_state_later)
 
     def _client_added_handler(self, sink, fd):
         self.log('[fd %5d] client_added_handler', fd)