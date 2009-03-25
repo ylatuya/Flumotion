ylatuya/Flumotion

commit 9969dc8726daa2f62a1d160c991a63c616011713

* bin/Makefile.am: 	* bin/flumotion-admin-command: 	* configure.ac: 	* flumotion/admin/command/common.py (added): 	* flumotion/admin/command/worker.py (added): 	* flumotion/admin/command/manager.py (added): 	* flumotion/admin/command/component.py (added): 	* flumotion/admin/command/mains.py (added): 	* flumotion/admin/command/Makefile.am: 	  Add new flumotion-admin-command, better abstracted, and using command 	  class.  This will replace flumotion-command. 	  Closes #725.
git-svn-id: svn+ssh://code.fluendo.com/flumotion/flumotion/trunk@7224 2b0047a9-a6d8-0310-accf-f7200b2a168c

flumotion/admin/command/component.py
@@ -1,0 +1,342 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+"""
+component commands
+"""
+
+from twisted.python import failure
+
+from flumotion.common import errors, planet, log
+from flumotion.common import common as fccommon
+from flumotion.monitor.nagios import util
+from flumotion.common.planet import moods
+
+from flumotion.admin.command import common
+
+__version__ = "$Rev: 6562 $"
+
+
+class Delete(common.AdminCommand):
+    description = "Delete a component."
+
+    def doCallback(self, args):
+        if not self.componentId:
+            common.errorRaise("Please specify a component id "
+                "with 'component -i [component-id]'")
+
+        p = self.parentCommand
+        d = self.getRootCommand().adminModel.callRemote('deleteComponent',
+            self.parentCommand.componentState)
+
+        def cb(result):
+            self.stdout.write("Deleted component.\n")
+
+        def eb(failure):
+            if failure.trap(errors.ComponentMoodError):
+                common.errorRaise("Component '%s' is in the wrong mood." %
+                    self.parentCommand.componentId)
+            else:
+                common.errorRaise(log.getFailureMessage(failure))
+
+        d.addCallback(cb)
+        d.addErrback(eb)
+
+        return d
+
+
+class Invoke(common.AdminCommand):
+    usage = "invoke [method-name] [arguments]"
+    summary = "invoke a method on a component"
+    description = """Invoke a method on a component.
+%s
+For a list of methods that can be invoked, see the component's medium class
+and its remote_* methods.
+
+Examples: getConfig, setFluDebug""" % common.ARGUMENTS_DESCRIPTION
+
+    def doCallback(self, args):
+        if not self.componentId:
+            common.errorRaise("Please specify a component id "
+                "with 'component -i [component-id]'")
+
+        try:
+            methodName = args[0]
+        except IndexError:
+            common.errorRaise('Please specify a method name to invoke.')
+        if len(args) > 1:
+            args = common.parseTypedArgs(args[1], args[2:])
+            if args is None:
+                common.errorRaise('Could not parse arguments.')
+        else:
+            args = []
+
+        p = self.parentCommand
+        d = self.getRootCommand().adminModel.componentCallRemote(
+            self.parentCommand.componentState, methodName, *args)
+
+        def cb(result):
+            import pprint
+            self.stdout.write("Invoking '%s' on '%s' returned:\n%s\n" % (
+                methodName, p.componentId, pprint.pformat(result)))
+
+        def eb(failure):
+            if failure.check(errors.NoMethodError):
+                common.errorRaise("No method '%s' on component '%s'." % (
+                    methodName, p.componentId))
+            elif failure.check(errors.SleepingComponentError):
+                common.errorRaise(
+                    "Component '%s' is sleeping." % p.componentId)
+            else:
+                common.errorRaise(log.getFailureMessage(failure))
+
+        d.addCallback(cb)
+        d.addErrback(eb)
+
+        return d
+
+
+class List(common.AdminCommand):
+    description = "List components."
+
+    def doCallback(self, args):
+        p = self.parentCommand
+        a = p.planetState.get('atmosphere')
+        if a.get('components'):
+            self.stdout.write('atmosphere:\n')
+            for c in a.get('components'):
+                self.stdout.write('    ' + c.get('name') + '\n')
+
+        for f in p.planetState.get('flows'):
+            if f.get('components'):
+                self.stdout.write('%s flow:\n' % f.get('name'))
+                for c in f.get('components'):
+                    self.stdout.write('    ' + c.get('name') + '\n')
+
+
+class Mood(common.AdminCommand):
+    description = "Check the mood of a component."
+
+    def doCallback(self, args):
+        if not self.componentId:
+            common.errorRaise("Please specify a component id "
+                "with 'component -i [component-id]'")
+
+        p = self.parentCommand
+        moodValue = p.componentState.get('mood')
+        moodName = planet.moods.get(moodValue).name
+        self.stdout.write("Component '%s' is %s.\n" % (p.componentId,
+            moodName))
+
+
+class PropertyGet(common.AdminCommand):
+    description = "Get a property of a component."
+    name = 'get'
+
+    def do(self, args):
+        if not args:
+            return errorReturn('Please specify a property to get.')
+
+        self._propertyName = args[0]
+
+        return common.AdminCommand.do(self, args)
+
+    def doCallback(self, args):
+        u = self.parentCommand.uiState
+        name = self._propertyName
+
+        if not u.hasKey(name):
+            common.errorRaise("Component '%s' does not have property '%s'." % (
+                self.parentCommand.parentCommand.componentId, name))
+
+        self.stdout.write("Property '%s' is '%r'.\n" % (
+            name, u.get(name)))
+
+
+class PropertyList(common.AdminCommand):
+    description = "List properties of a component."
+    name = 'list'
+
+    def doCallback(self, args):
+        l = self.parentCommand.uiState.keys()
+        l.sort()
+        self.stdout.write('Properties:\n')
+        for p in l:
+            self.stdout.write('- %s\n' % p)
+
+# FIXME: why is this called property when it really is about ui state ?
+
+
+class Property(util.LogCommand):
+    """
+    @param uiState: the ui state of the component; set after logging in.
+    """
+
+    description = "Act on properties of a component."
+
+    subCommandClasses = [PropertyGet, PropertyList]
+
+    def handleOptions(self, options):
+        if not self.componentId:
+            common.errorRaise("Please specify a component id "
+                "with 'component -i [component-id]'")
+
+        # call our callback after connecting
+        d = self.getRootCommand().managerDeferred
+        d.addCallback(self._callback)
+        d.addErrback(self._errback)
+
+    def _callback(self, result):
+
+        def getUIStateCb(uiState):
+            self.uiState = uiState
+
+        componentCommand = self.parentCommand
+        model = componentCommand.parentCommand.adminModel
+        d = model.componentCallRemote(
+            componentCommand.componentState, 'getUIState')
+        d.addCallback(getUIStateCb)
+        return d
+
+    def _errback(self, failure):
+        failure.trap(errors.SleepingComponentError)
+        common.errorRaise("Component '%s' is sleeping." %
+            self.parentCommand.componentId)
+
+
+class Start(common.AdminCommand):
+    description = "Start a component."
+
+    def doCallback(self, args):
+        if not self.componentId:
+            common.errorRaise("Please specify a component id "
+                "with 'component -i [component-id]'")
+
+        p = self.parentCommand
+        moodValue = p.componentState.get('mood')
+        if moodValue == moods.happy.value:
+            self.stdout.write("Component is already happy.\n")
+            return 0
+
+        d = self.getRootCommand().adminModel.callRemote('componentStart',
+            self.parentCommand.componentState)
+
+        def cb(result):
+            self.stdout.write("Started component.\n")
+
+        def eb(failure):
+            if failure.trap(errors.ComponentMoodError):
+                common.errorRaise("Component '%s' is in the wrong mood." %
+                    self.parentCommand.componentId)
+            else:
+                common.errorRaise(log.getFailureMessage(failure))
+
+        d.addCallback(cb)
+        d.addErrback(eb)
+
+        return d
+
+
+class Stop(common.AdminCommand):
+    description = "Stop a component."
+
+    def doCallback(self, args):
+        if not self.componentId:
+            common.errorRaise("Please specify a component id "
+                "with 'component -i [component-id]'")
+
+        p = self.parentCommand
+        moodValue = p.componentState.get('mood')
+        if moodValue == moods.sleeping.value:
+            self.stdout.write("Component is already sleeping.\n")
+            return 0
+
+        d = self.getRootCommand().adminModel.callRemote('componentStop',
+            self.parentCommand.componentState)
+
+        def cb(result):
+            self.stdout.write("Stopped component.\n")
+
+        def eb(failure):
+            if failure.trap(errors.ComponentMoodError):
+                common.errorRaise("Component '%s' is in the wrong mood." %
+                    self.parentCommand.componentId)
+            else:
+                common.errorRaise(log.getFailureMessage(failure))
+
+        d.addCallback(cb)
+        d.addErrback(eb)
+
+        return d
+
+
+class Component(util.LogCommand):
+    """
+    @ivar  componentId:    the component id, passed as an argument
+    @ivar  componentState: the component state; set when logged in to manager.
+    @type  componentState: L{flumotion.common.state.AdminComponentState}
+    @ivar  planetState:    the planet state; set when logged in to manager.
+    @type  planetState:    L{flumotion.common.state.AdminPlanetState}
+    """
+    description = "Act on a component."
+    usage = "component -i [component id]"
+
+    subCommandClasses = [Delete, Invoke, List, Mood, Property, Start, Stop]
+
+    componentId = None
+    componentState = None
+    planetState = None
+
+    def addOptions(self):
+        self.parser.add_option('-i', '--component-id',
+            action="store", dest="componentId",
+            help="component id of the component")
+
+    def handleOptions(self, options):
+        self.componentId = options.componentId
+        # call our callback after connecting
+        self.getRootCommand().managerDeferred.addCallback(self._callback)
+
+    def _callback(self, result):
+        d = self.parentCommand.adminModel.callRemote('getPlanetState')
+
+        def gotPlanetStateCb(result):
+            self.planetState = result
+            self.debug('gotPlanetStateCb')
+
+            # only get componentState if we got passed an argument for it
+            if not self.componentId:
+                return
+
+            try:
+                self.componentState = util.findComponent(result,
+                    self.componentId)
+            except Exception, e:
+                self.debug(log.getExceptionMessage(e))
+                common.errorRaise("Invalid component id '%s'" %
+                    self.componentId)
+            self.debug('gotPlanetStateCb')
+            if not self.componentState:
+                common.errorRaise('Could not find component %s' %
+                    self.componentId)
+
+        d.addCallback(gotPlanetStateCb)
+        return d

bin/Makefile.am
@@ -1,5 +1,6 @@
 bin_SCRIPTS = \
 	flumotion-admin \
+	flumotion-admin-command \
 	flumotion-admin-text \
 	flumotion-command \
 	flumotion-inspect \

bin/flumotion-admin-command
@@ -1,0 +1,50 @@
+#!/usr/bin/python
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+import os
+import sys
+
+# Variable templates
+LIBDIR = '/home/thomas/flu/tmp/prefix/lib'
+# FIXME: rename ot main
+PROGRAM_PATH = 'flumotion.admin.command.mains.main'
+NEEDS_GTK=False
+NEEDS_GST=False
+
+try:
+    # setup the project root
+    dir = os.path.dirname(os.path.abspath(__file__))
+    if os.path.exists(os.path.join(dir, '..', 'flumotion', '.svn')):
+        root = os.path.split(dir)[0]
+    else:
+        root = os.path.join(LIBDIR, 'flumotion', 'python')
+    sys.path.insert(0, root)
+
+    import warnings
+    warnings.filterwarnings('ignore', category=UserWarning)
+
+    # and boot!
+    from flumotion.common import boot
+    boot.boot(PROGRAM_PATH, gtk=NEEDS_GTK, gst=NEEDS_GST)
+
+except KeyboardInterrupt:
+    print 'Interrupted'

configure.ac
@@ -191,6 +191,7 @@ AC_CONFIG_FILES([env], [chmod +x env])
 AC_CONFIG_FILES([bin/flumotion], [chmod +x bin/flumotion])
 AC_CONFIG_FILES([bin/flumotion-admin], [chmod +x bin/flumotion-admin])
 AC_CONFIG_FILES([bin/flumotion-admin-text], [chmod +x bin/flumotion-admin-text])
+AC_CONFIG_FILES([bin/flumotion-admin-command], [chmod +x bin/flumotion-admin-command])
 AC_CONFIG_FILES([bin/flumotion-command], [chmod +x bin/flumotion-command])
 AC_CONFIG_FILES([bin/flumotion-inspect], [chmod +x bin/flumotion-inspect])
 AC_CONFIG_FILES([bin/flumotion-job], [chmod +x bin/flumotion-job])

flumotion/admin/command/Makefile.am
@@ -4,8 +4,13 @@ componentdir = $(libdir)/flumotion/python/flumotion/admin/command
 component_PYTHON = \
 	__init__.py \
 	commands.py \
+	common.py \
+	component.py \
 	main.py \
-	utils.py
+	mains.py \
+	manager.py \
+	utils.py \
+	worker.py
 
 clean-local:
 	rm -rf *.pyc *.pyo

flumotion/admin/command/common.py
@@ -1,0 +1,156 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+"""
+common functionality for flumotion-admin-command
+"""
+
+import sys
+
+# we need to have the unjelliers registered
+# FIXME: why is this not in flumotion.admin.admin ?
+from flumotion.common import componentui, common
+
+# FIXME: move
+from flumotion.monitor.nagios import util
+
+__version__ = "$Rev: 6562 $"
+
+# explain the complicated arguments system for the invoke methods
+ARGUMENTS_DESCRIPTION = """
+Arguments to the method are passed using an argument list string, and the
+arguments (matching the argument list string).
+
+Example: "method ss one two" would invoke remote_method("one", "two")
+"""
+
+# code copied over from old flumotion-command
+
+
+class ParseException(Exception):
+    pass
+
+# FIXME: don't print darn it
+
+
+def parseTypedArgs(spec, args):
+
+    def _readFile(filename):
+        try:
+            f = open(filename)
+            contents = f.read()
+            f.close()
+            return contents
+        except OSError:
+            raise ParseException("Failed to read file %s" % (filename, ))
+
+    def _doParseTypedArgs(spec, args):
+        accum = []
+        while spec:
+            argtype = spec.pop(0)
+            parsers = {'i': int, 's': str, 'b': common.strToBool,
+                'F': _readFile}
+            if argtype == ')':
+                return tuple(accum)
+            elif argtype == '(':
+                accum.append(_do_parse_typed_args(spec, args))
+            elif argtype == '}':
+                return dict(accum)
+            elif argtype == '{':
+                accum.append(_do_parse_typed_args(spec, args))
+            elif argtype == ']':
+                return accum
+            elif argtype == '[':
+                accum.append(_doParseTypedArgs(spec, args))
+            elif argtype not in parsers:
+                raise ParseException('Unknown argument type: %r'
+                                     % argtype)
+            else:
+                parser = parsers[argtype]
+                try:
+                    arg = args.pop(0)
+                except IndexError:
+                    raise ParseException('Missing argument of type %r'
+                                         % parser)
+                try:
+                    accum.append(parser(arg))
+                except Exception, e:
+                    raise ParseException('Failed to parse %s as %r: %s'
+                                         % (arg, parser, e))
+
+    spec = list(spec) + [')']
+    args = list(args)
+
+    try:
+        res = _doParseTypedArgs(spec, args)
+    except ParseException, e:
+        print e.args[0]
+        return None
+
+    if args:
+        print 'Left over arguments:', args
+        return None
+    else:
+        return res
+
+# helper subclass for leaf commands
+
+
+class AdminCommand(util.LogCommand):
+
+    def do(self, args):
+        # call our callback after connecting
+        self.getRootCommand().managerDeferred.addCallback(self._callback, args)
+
+    def _callback(self, result, args):
+        return self.doCallback(args)
+
+    def doCallback(self, args):
+        """
+        Subclasses should implement this as an alternative to the normal do
+        method. It will be called after a connection to the manager is made.
+
+        Don't forget to return a deferred you create to properly chain
+        execution.
+        """
+        raise NotImplementedError(
+            "subclass %r should implement doCallback" % self.__class__)
+
+
+class Exited(Exception):
+    """
+    Raised when the code wants the program to exit with a return value and
+    a message.
+    """
+
+    def __init__(self, code, msg=None):
+        self.args = (code, msg)
+        self.code = code
+        self.msg = msg
+
+
+def errorRaise(msg):
+    raise Exited(1, "ERROR: " + msg)
+
+
+def errorReturn(msg):
+    sys.stderr.write("ERROR: " + msg + '\n')
+    return 1

flumotion/admin/command/mains.py
@@ -1,0 +1,205 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+"""
+main for flumotion-admin-command
+"""
+
+import sys
+
+from twisted.internet import reactor, defer
+
+from flumotion.common import errors, log
+from flumotion.admin import connections, admin
+
+from flumotion.monitor.nagios import util
+
+from flumotion.admin.command import component, manager, worker, common
+
+from flumotion.common.common import version
+
+__version__ = "$Rev: 6562 $"
+
+# Because we run a reactor and use deferreds, the flow is slightly different
+# from the usual Command flow.
+
+# Nagios will first create a managerDeferred instance variable, which will
+# allow subcommands to hook into the connection and schedule callbacks.
+
+# Nagios will then parse the command line, allowing all subcommands to
+# hook into this step with their respective handleOptions/parse/do methods.
+
+# Subcommands are expected to use the ok/warning/critical methods to report
+# a message and set the exit state.
+
+# The Nagios root command will take care of stopping the reactor and returning
+# the exit value.
+
+
+class Command(util.LogCommand):
+    usage = "%prog %command"
+    description = "Run commands on Flumotion manager."
+
+    managerDeferred = None # deferred that fires upon connection
+    adminModel = None      # AdminModel connected to the manager
+
+    subCommandClasses = [component.Component, manager.Manager, worker.Worker]
+
+    def addOptions(self):
+        self.parser.add_option('-v', '--version',
+            action="store_true", dest="version",
+            help="show version information")
+        default = "user:test@localhost:7531"
+        self.parser.add_option('-m', '--manager',
+            action="store", type="string", dest="manager",
+            help="the manager connection string, " \
+                 "in the form [username[:password]@]host:port " \
+                 "(defaults to %s)" % default,
+            default=default)
+        self.parser.add_option('-T', '--transport',
+            action="store_true", dest="transport",
+            help="transport protocol to use (tcp/ssl) [default ssl]",
+            default="ssl")
+
+    def handleOptions(self, options):
+        self.debug('command: handleOptions')
+        if options.version:
+            print version("flumotion-admin-command")
+            return 0
+
+    def parse(self, argv):
+        # instantiated here so our subcommands can chain to it
+        self.managerDeferred = defer.Deferred()
+
+        self.debug('parse: chain up')
+        # chain up to parent first
+        # all subcommands will have a chance to chain up to the deferred
+        ret = util.LogCommand.parse(self, argv)
+
+        if ret is None:
+            self.debug('parse returned None, help/usage printed')
+            return ret
+
+        if ret:
+            self.debug('parse returned %r' % ret)
+            return ret
+
+        if self.parser.help_printed or self.parser.usage_printed:
+            return 0
+
+        # now connect
+        self.debug('parse: connect')
+        self.connect(self.options)
+
+        # chain up an exit after our child commands have had the chance.
+
+        def cb(result):
+            self.debug('parse: cb: done')
+            reactor.callLater(0, reactor.stop)
+
+        def eb(failure):
+            self.debug('parse: eb: failure %s' %
+                log.getFailureMessage(failure))
+            if failure.check(common.Exited):
+                sys.stderr.write(failure.value.msg + '\n')
+                reactor.exitStatus = failure.value.code
+            else:
+                sys.stderr.write(log.getFailureMessage(failure) + '\n')
+                reactor.exitStatus = 1
+
+            reactor.callLater(0, reactor.stop)
+            return
+
+        self.managerDeferred.addCallback(cb)
+        self.managerDeferred.addErrback(eb)
+
+        # now run the reactor
+        self.debug('parse: run the reactor')
+        self.run()
+        self.debug('parse: ran the reactor')
+
+        return reactor.exitStatus
+
+    def run(self):
+        """
+        Run the reactor.
+
+        Resets .exitStatus, and returns its value after running the reactor.
+        """
+        # run the reactor
+
+        self.debug('running reactor')
+        # We cheat by putting the exit code in the reactor.
+        reactor.exitStatus = 0
+        reactor.run()
+        self.debug('ran reactor')
+
+        return reactor.exitStatus
+
+    def connect(self, options):
+        connection = connections.parsePBConnectionInfo(options.manager,
+                                               options.transport == 'ssl')
+
+        # platform-3/trunk compatibility stuff to guard against
+        # gratuitous changes
+        try:
+            # platform-3
+            self.adminModel = admin.AdminModel(connection.authenticator)
+            self.debug("code is platform-3")
+        except TypeError:
+            # trunk
+            self.adminModel = admin.AdminModel()
+            self.debug("code is trunk")
+
+        if hasattr(self.adminModel, 'connectToHost'):
+            # platform-3
+            d = self.adminModel.connectToHost(connection.host,
+                connection.port, not connection.use_ssl)
+        else:
+            d = self.adminModel.connectToManager(connection)
+
+        d.addCallback(self._connectedCb)
+        d.addErrback(self._connectedEb)
+
+    def _connectedCb(self, result):
+        self.debug('Connected to manager.')
+        self.managerDeferred.callback(result)
+
+    def _connectedEb(self, failure):
+        if failure.check(errors.ConnectionFailedError):
+            sys.stderr.write("Unable to connect to manager.\n")
+        if failure.check(errors.ConnectionRefusedError):
+            sys.stderr.write("Manager refused connection.\n")
+        self.managerDeferred.errback(failure)
+
+
+def main(args):
+    c = Command()
+    try:
+        ret = c.parse(args[1:])
+    except common.Exited, e:
+        ret = e.code
+        if ret == 0:
+            sys.stdout.write(e.msg + '\n')
+        else:
+            sys.stderr.write(e.msg + '\n')
+
+    return ret

commit 6f4918a8cc708443a95cc6d507f99ca0ee0785bb

* flumotion/admin/command/component.py: 	Further fix flumotion-command, after the fallout in r7224. 	See also r7691.
git-svn-id: svn+ssh://code.fluendo.com/flumotion/flumotion/trunk@7807 2b0047a9-a6d8-0310-accf-f7200b2a168c

flumotion/admin/command/component.py
@@ -39,7 +39,7 @@ class Delete(common.AdminCommand):
     description = "Delete a component."
 
     def doCallback(self, args):
-        if not self.componentId:
+        if not self.parentCommand.componentId:
             common.errorRaise("Please specify a component id "
                 "with 'component -i [component-id]'")
 
@@ -225,7 +225,7 @@ class Start(common.AdminCommand):
     description = "Start a component."
 
     def doCallback(self, args):
-        if not self.componentId:
+        if not self.parentCommand.componentId:
             common.errorRaise("Please specify a component id "
                 "with 'component -i [component-id]'")
 
@@ -258,7 +258,7 @@ class Stop(common.AdminCommand):
     description = "Stop a component."
 
     def doCallback(self, args):
-        if not self.componentId:
+        if not self.parentCommand.componentId:
             common.errorRaise("Please specify a component id "
                 "with 'component -i [component-id]'")
 